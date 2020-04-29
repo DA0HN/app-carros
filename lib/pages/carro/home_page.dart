c471b9538ba7cabd18ba54447bafe37596cf3e08
Gabriel Honda <vegancraft13@gmail.com>
2020-04-29 11:55:45 -0400

    CircularProgressIndicator na home page

--- lib/pages/carro/home_page.dart
+++ lib/pages/carro/home_page.dart
@@ -21,8 +21,10 @@ class HomePage extends StatelessWidget {
     return FutureBuilder(
       future: futureData,
       builder: (BuildContext context, AsyncSnapshot snapshot) {
-        if( !snapshot.hasData) {
-          return Center(child: CircularProgressIndicator(),);
+        if (!snapshot.hasData) {
+          return Center(
+            child: CircularProgressIndicator(),
+          );
         }
         List<Carro> carros = snapshot.data;
         return _listView(carros);
@@ -32,55 +34,55 @@ class HomePage extends StatelessWidget {
 
   Container _listView(List<Carro> carros) {
     return Container(
-    child: ListView.builder(
-      padding: EdgeInsets.all(16),
-      itemCount: carros != null ? carros.length : 0,
-      itemBuilder: (context, index) {
-        Carro c = carros[index];
-        return Card(
-          color: Colors.grey[100],
-          child: Container(
-            padding: EdgeInsets.all(10),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: <Widget>[
-                Center(
-                  child: Image.network(
-                    c.urlFoto,
-                    width: 250,
+      child: ListView.builder(
+        padding: EdgeInsets.all(16),
+        itemCount: carros != null ? carros.length : 0,
+        itemBuilder: (context, index) {
+          Carro c = carros[index];
+          return Card(
+            color: Colors.grey[100],
+            child: Container(
+              padding: EdgeInsets.all(10),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: <Widget>[
+                  Center(
+                    child: Image.network(
+                      c.urlFoto,
+                      width: 250,
+                    ),
                   ),
-                ),
-                Text(
-                  c.nome,
-                  maxLines: 1,
-                  overflow: TextOverflow.ellipsis,
-                  style: TextStyle(fontSize: 25),
-                ),
-                Text(
-                  "descrição...",
-                  style: TextStyle(fontSize: 16),
-                ),
-                ButtonBarTheme(
-                  data: ButtonBarTheme.of(context),
-                  child: ButtonBar(
-                    children: <Widget>[
-                      FlatButton(
-                        child: const Text("DETALHES"),
-                        onPressed: () {},
-                      ),
-                      FlatButton(
-                        child: const Text("SHARE"),
-                        onPressed: () {},
-                      ),
-                    ],
+                  Text(
+                    c.nome,
+                    maxLines: 1,
+                    overflow: TextOverflow.ellipsis,
+                    style: TextStyle(fontSize: 25),
                   ),
-                )
-              ],
+                  Text(
+                    "descrição...",
+                    style: TextStyle(fontSize: 16),
+                  ),
+                  ButtonBarTheme(
+                    data: ButtonBarTheme.of(context),
+                    child: ButtonBar(
+                      children: <Widget>[
+                        FlatButton(
+                          child: const Text("DETALHES"),
+                          onPressed: () {},
+                        ),
+                        FlatButton(
+                          child: const Text("SHARE"),
+                          onPressed: () {},
+                        ),
+                      ],
+                    ),
+                  )
+                ],
+              ),
             ),
-          ),
-        );
-      },
-    ),
-  );
+          );
+        },
+      ),
+    );
   }
 }
